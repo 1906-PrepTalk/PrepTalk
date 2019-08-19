c808a5e87c5f838a6e2cfb2fb01f8fd7a5041c17
Daphne Lee <daphnelee92@gmail.com>
2019-08-19 15:51:05 -0400

    fix(models/face, faceRecording): code review follow up

--- server/db/models/face.js
+++ server/db/models/face.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Face = db.define('face', {
-    data: {
-        type: Sequelize.JSON
-    }
+  // data: {
+  //     type: Sequelize.JSON
+  // }
+  expressions: {
+    type: Sequelize.ENUM
+  }
 })
 
 module.exports = Face
