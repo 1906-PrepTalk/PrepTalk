2c57263d7efdd6ad125eb795531114489b67de1f
Michael Chan <mchan5791@gmail.com>
2019-08-24 00:57:42 -0400

    testing credentials

--- server/transcribe.js
+++ server/transcribe.js
@@ -16,7 +16,7 @@ if (
 // Instantiates a client. If you don't specify credentials when constructing
 // the client, the client library will look for credentials in the
 // environment.
-const storage = new Storage()
+const storage = new Storage(process.env.GOOGLE_CLOUD_PROJECT, googCredentials)
 
 // Makes an authenticated API request.
 storage
