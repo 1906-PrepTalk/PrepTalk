9bce10e5f13a8ec4f1fb96669715aacf48630fd6
Michael Chan <mchan5791@gmail.com>
2019-08-24 00:17:10 -0400

    testing credentials

--- server/transcribe.js
+++ server/transcribe.js
@@ -5,7 +5,7 @@ const {Storage} = require('@google-cloud/storage')
 // Instantiates a client. If you don't specify credentials when constructing
 // the client, the client library will look for credentials in the
 // environment.
-const storage = new Storage()
+const storage = new Storage(googCredentials)
 
 // Makes an authenticated API request.
 storage
