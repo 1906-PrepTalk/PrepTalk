ef6925ff2ec362658ccd95eb4c0b5c3c50ff1e67
Michael Chan <mchan5791@gmail.com>
2019-08-29 15:45:18 -0400

    working heroku

--- server/transcribe.js
+++ server/transcribe.js
@@ -12,7 +12,6 @@ if (
 
 const {Storage} = require('@google-cloud/storage')
 const storage = new Storage()
-const gclient = new speech.SpeechClient()
 
 function store(bucketId, filename) {
   const gbucket = storage.bucket(bucketId)
@@ -35,39 +34,30 @@ function store(bucketId, filename) {
 /**
  * Send audio to transcription service Google Cloud Speech API
  */
-function transcribeAudio(googFilename) {
-  console.log('Here are the', googCredentials)
-  return gclient
-    .recognize({
-      config: {
-        encoding: 'FLAC',
-        languageCode: 'en-US'
-      },
-      audio: {
-        uri: googFilename
-      }
-    })
-    .then(data => {
-      console.log('This is data:', data)
-      const res = data[0]
-      console.log('This is res:', res)
-      return res.promise()
-    })
-    .then(data => {
-      const res = data[0]
-      // const metadata = data[1]
-      const transcript = res.results
-        .map(r => {
-          console.log(r.alternatives)
-          return r.alternatives[0].transcript.trim()
-        })
-        .join('\n')
-      return transcript
-    })
-    .catch(err => {
-      console.log('This is googFileName', googFilename)
-      console.log(`Error transcribing audio. Reason: ${err}`)
-    })
+const transcribeAudio = async googFilename => {
+  try {
+    const gclient = new speech.SpeechClient()
+    const config = {
+      encoding: 'LINEAR16',
+      languageCode: 'en-US'
+    }
+    const audio = {
+      uri: googFilename
+    }
+    const request = {
+      config: config,
+      audio: audio
+    }
+    // Detects speech in the audio file
+    const [response] = await gclient.recognize(request)
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n')
+    console.log(`Transcription: `, transcription)
+    return transcription
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 // export
