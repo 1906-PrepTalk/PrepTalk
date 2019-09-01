efd4768c70bc0e179d8ec72d829835dfa46eae0d
Michael Chan <mchan5791@gmail.com>
2019-09-01 11:42:56 -0400

    revert donut css

--- client/components/faceAnalysis.js
+++ client/components/faceAnalysis.js
@@ -30,7 +30,6 @@ class FaceAnalysis extends Component {
 
   componentDidMount() {
     const {archiveId} = this.props.match.params
-    this.setState({button: true})
     this.props.getArchivedVideo(archiveId)
     this.props.getAllVideos(this.props.userId)
     this.props.getTranscript(archiveId)
